27dc47e4380b076442b5a4c0d1a530d4524e5a5a
Albert Allagulov <Albert.Allagulov@nexign.com>
2025-04-15 11:44:38 +0300

    add name to header and input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,42 @@
 import { Component } from '@angular/core';
-import { TuiButton, TuiTitle, TuiRoot } from '@taiga-ui/core';
-import { TuiBadgeNotification } from '@taiga-ui/kit';
+import { FormsModule } from '@angular/forms';
+import {
+  TuiButton,
+  TuiTitle,
+  TuiRoot,
+  TuiIcon,
+  TuiTextfield,
+} from '@taiga-ui/core';
+import { TuiBadgedContent, TuiBadgeNotification } from '@taiga-ui/kit';
 import { TuiHeader } from '@taiga-ui/layout';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [TuiRoot, TuiBadgeNotification, TuiButton, TuiHeader, TuiTitle],
+  imports: [
+    FormsModule,
+    TuiRoot,
+    TuiBadgeNotification,
+    TuiButton,
+    TuiHeader,
+    TuiTitle,
+    TuiBadgedContent,
+    TuiIcon,
+    TuiTextfield,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList('Albert', [new TodoItem('Go for run', true)]);
+
+  value = '';
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  clear() {}
 }
