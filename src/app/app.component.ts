3c3cf878b62c15da48267dacc69b8c5fc2560a79
Albert Allagulov <Albert.Allagulov@nexign.com>
2025-04-15 13:14:35 +0300

    finish todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
   TuiButton,
@@ -7,8 +8,14 @@ import {
   TuiIcon,
   TuiTextfield,
 } from '@taiga-ui/core';
-import { TuiBadgedContent, TuiBadgeNotification } from '@taiga-ui/kit';
-import { TuiHeader } from '@taiga-ui/layout';
+import {
+  TuiBadgedContent,
+  TuiBadgeNotification,
+  TuiCheckbox,
+  TuiSwitch,
+} from '@taiga-ui/kit';
+import { TuiHeader, TuiCell } from '@taiga-ui/layout';
+import { TuiTable } from '@taiga-ui/addon-table';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 
@@ -25,18 +32,42 @@ import { TodoItem } from './TodoItem';
     TuiBadgedContent,
     TuiIcon,
     TuiTextfield,
+    TuiTable,
+    NgForOf,
+    TuiCheckbox,
+    TuiSwitch,
+    TuiCell,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private list = new TodoList('Albert', [new TodoItem('Go for run', true)]);
+  private list = new TodoList('Albert', [
+    new TodoItem('Go for run', true),
+    new TodoItem('Get flowers'),
+    new TodoItem('Collect tickets'),
+  ]);
 
-  value = '';
+  showComplete: boolean = false;
 
   get username(): string {
     return this.list.user;
   }
 
-  clear() {}
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(
+      (item) => this.showComplete || !item.complete,
+    );
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter((item) => this.showComplete || !item.complete)
+      .length;
+  }
+
+  addItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
 }
