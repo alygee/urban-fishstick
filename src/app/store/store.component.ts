4faef3b60578137499a37f0b4fd33c45b12aa428
Albert Allagulov <Albert.Allagulov@nexign.com>
2025-04-17 23:28:03 +0300

    SportsStore begining

---
+++ src/app/store/store.component.ts
@@ -0,0 +1,94 @@
+import { Component, Signal, computed, signal } from '@angular/core';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
+import { FormControl, FormGroup } from '@angular/forms';
+import { tuiItemsHandlersProvider } from '@taiga-ui/core';
+
+interface PageSize {
+  readonly value: number;
+  readonly label: string;
+}
+
+@Component({
+  standalone: false,
+  selector: 'store',
+  templateUrl: 'store.component.html',
+  styleUrl: './store.component.less',
+  providers: [
+    tuiItemsHandlersProvider({
+      stringify: signal((x: any) => {
+        if (typeof x === 'number') {
+          return x + ' per Page';
+        }
+        return x() + ' per Page';
+      }),
+    }),
+  ],
+})
+export class StoreComponent {
+  products: Signal<Product[]>;
+  categories: Signal<string[]>;
+  selectedCategory = signal<string | undefined>(undefined);
+  productsPerPage = signal(4);
+  selectedPage = signal(1);
+  pagedProducts: Signal<Product[]>;
+  //pageNumbers: Signal<number[]>;
+  pageCount: Signal<number>;
+  protected readonly pageSizes = [3, 4, 6, 8];
+
+  categoryForm = new FormGroup({
+    categoryValue: new FormControl('home'),
+  });
+
+  constructor(private repository: ProductRepository) {
+    this.products = computed(() => {
+      if (this.selectedCategory() == undefined) {
+        return this.repository.products();
+      } else {
+        return this.repository
+          .products()
+          .filter((p) => p.category === this.selectedCategory());
+      }
+    });
+    this.categories = repository.categories;
+    let pageIndex = computed(() => {
+      return (this.selectedPage() - 1) * this.productsPerPage();
+    });
+    this.pagedProducts = computed(() => {
+      return this.products().slice(
+        pageIndex(),
+        pageIndex() + this.productsPerPage(),
+      );
+    });
+    // this.pageNumbers = computed(() => {
+    //     return Array(Math.ceil(this.products().length
+    //             / this.productsPerPage()))
+    //         .fill(0).map((x, i) => i + 1);
+    // });
+    this.pageCount = computed(() => {
+      return Math.ceil(this.products().length / this.productsPerPage());
+    });
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory.set(newCategory);
+    this.changePage(1);
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage.set(newPage);
+  }
+
+  protected goToPage(index: number): void {
+    this.selectedPage.set(index + 1);
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage.set(Number(newSize));
+    this.changePage(1);
+  }
+
+  piu(cat: any) {
+    console.log(cat);
+  }
+}
---
+++ src/app/store/store.module.ts
@@ -0,0 +1,67 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgForOf, NgIf } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ModelModule } from '../model/model.module';
+import { StoreComponent } from './store.component';
+import { CounterDirective } from './counter.directive';
+import {
+  TuiButton,
+  TuiIcon,
+  TuiLink,
+  TuiTitle,
+  TuiGroup,
+  tuiItemsHandlersProvider,
+  TuiTextfield,
+} from '@taiga-ui/core';
+import {
+  TuiAvatar,
+  TuiBadge,
+  TuiBadgeNotification,
+  TuiSensitive,
+  TuiTiles,
+  TuiBlock,
+  TuiRadio,
+  TuiTooltip,
+  TuiPagination,
+  TuiChevron,
+  TuiDataListWrapper,
+  TuiSelect,
+} from '@taiga-ui/kit';
+import { TuiHeader, TuiCell } from '@taiga-ui/layout';
+
+@NgModule({
+  imports: [
+    ModelModule,
+    BrowserModule,
+    FormsModule,
+    NgForOf,
+    NgIf,
+    ReactiveFormsModule,
+    TuiBlock,
+    TuiAvatar,
+    TuiBadge,
+    TuiBadgeNotification,
+    TuiButton,
+    TuiHeader,
+    TuiIcon,
+    TuiLink,
+    TuiSensitive,
+    TuiTitle,
+    TuiTooltip,
+    TuiTiles as any,
+    TuiRadio,
+    TuiGroup,
+    TuiCell,
+    TuiPagination,
+    TuiChevron,
+    TuiDataListWrapper,
+    TuiSelect,
+    TuiTextfield,
+  ],
+  declarations: [StoreComponent, CounterDirective],
+  exports: [StoreComponent],
+})
+export class StoreModule {}
